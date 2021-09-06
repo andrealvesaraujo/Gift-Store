3bbecfb3ac7a549a11630b96cf450851e3c89aca
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-09-06 17:23:37 -0300

    Add all sections and it's info

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,23 @@ import React from "react";
 class Header extends React.Component {
     render() {
         return ( 
-            <section>Header</section>
+            <section>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu massa sit amet augue consectetur ullamcorper non ac massa.</p>
+                <button>Botão</button>
+            </section>
+        );
+    }
+}
+
+class Card extends React.Component {
+    render() {
+        return ( 
+            <div>
+                <div></div>
+                <div>Titulo do Card</div>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.</div>
+            </div>
         );
     }
 }
@@ -11,7 +27,30 @@ class Header extends React.Component {
 class Cards extends React.Component {
     render() {
         return ( 
-            <section>Card</section>
+            <section>
+                <ul>
+                    <li><Card/></li>
+                    <li><Card/></li>
+                    <li><Card/></li>
+                    <li><Card/></li>
+                    <li><Card/></li>
+                    <li><Card/></li>
+                </ul>
+            </section>
+        );
+    }
+}
+
+class Form extends React.Component {
+    render() {
+        return ( 
+            <form>
+                <input placeholder="Nome"></input>
+                <input placeholder="E-mail"></input>
+                <input placeholder="Telefone"></input>
+                <input placeholder="Mensagem"></input>
+                <button>Enviar Mensagem</button>
+            </form>
         );
     }
 }
@@ -19,7 +58,12 @@ class Cards extends React.Component {
 class Contato extends React.Component {
     render() {
         return ( 
-            <section>Contato Form</section>
+            <section>
+                <div>
+                    <h3>Entre em contato </h3>
+                    <Form/>
+                </div>
+            </section>
         );
     }
 }
