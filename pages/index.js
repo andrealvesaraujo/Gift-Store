bde3af362c496ed11dd45d9228a20cc077f8a02c
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-09-06 17:16:07 -0300

    Create small components for sections of page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
 import React from "react";
-export default class Home extends React.Component {
+
+class Header extends React.Component {
+    render() {
+        return ( 
+            <section>Header</section>
+        );
+    }
+}
+
+class Cards extends React.Component {
+    render() {
+        return ( 
+            <section>Card</section>
+        );
+    }
+}
+
+class Contato extends React.Component {
+    render() {
+        return ( 
+            <section>Contato Form</section>
+        );
+    }
+}
+export default class Principal extends React.Component {
     
     render() {
         return ( 
-            <div>HOME</div> 
+            <main>
+                <Header />
+                <Cards />
+                <Contato />
+            </main>
         );
     }
 }
